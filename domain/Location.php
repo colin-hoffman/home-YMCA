cbc258a672d20b3f5fd623a1abeb3b43a97d0988
Tim Werme <twerme@mail.umw.edu>
2022-04-02 22:54:53 -0400

    Added basic class and constructor values to Reservation.php and Location.php

--- domain/Location.php
+++ domain/Location.php
@@ -1,1 +1,23 @@
+<?php
+/*
+ * Created April 1 2022
+ * Version #1
+ */
 
+class Location {
+	private $id;
+	private $name;
+	private $start_time;
+	private $end_time;
+	private $capacity;
+
+	function __construct($i, $n, $s, $e, $c) {
+
+		$this->id = $i;
+		$this->name = $n;
+		$this->start_time = $s;
+		$this->end_time = $e;
+		$this->capacity = $c;
+	}
+	
+?>
--- domain/Reservation.php
+++ domain/Reservation.php
@@ -1,1 +1,25 @@
+<?php
 
+
+
+class Reservation {
+	private $id;
+	private $count;
+	private $child_first;
+	private $child_last;
+	private $location;
+	private $date;
+	private $time;
+	private $guardian_email;
+
+	function __construct($c, $first, $last, $loc, $d, $t, $g) {
+		$this->id = $first . $g;
+		$this->count = $c;
+		$this->child_first = $first;
+		$this->child_last = $last;
+		$this->location = $loc;
+		$this->date = $d;
+		$this->time = $t;
+		$this->guardian_email = $g;
+	}
+?>
