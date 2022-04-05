24f5ca6f1024fb9f9a98c5701a02e566f5ea95e0
colin-hoffman <66750992+colin-hoffman@users.noreply.github.com>
2022-04-05 16:23:36 -0400

    Edited Checkbox Page
    Created table, filled it with reservation data, and added checkboxes next to each reservation

--- checkbox.php
+++ checkbox.php
@@ -1,11 +1,10 @@
 <?php
-include_once('dbinfo.php');
-if (isset($_POST['something'])) {
-    foreach($_POST['check'] AS $value) {
-        $stmnt = $db->prepare();
-    }
-}
+include_once('database/dbinfo.php');
+$mysqli = connect();
+$resultSet = $mysqli->query("SELECT * FROM dbreservation");
 ?>
+
+
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <script>
 $(document).ready(function() {
@@ -20,27 +19,28 @@ $(document).ready(function() {
 <link rel="stylesheet" href="stylesheetForm.css">
 -->
 <table border="1" cellpadding="5" cellspacing="0">
-    <th><input type="checkbox" id="select-all"/></th>
-    <th>Location</th>
-    <th>Child</th>
-    <th>Date</th>
-    <th>Time</th>
-    <!-- Need proper select statement for reservations table below
+    <t>
+        <th><input type="checkbox" id="select-all"/></th>
+        <th>ID</th>
+        <th>Location</th>
+        <th>Child</th>
+        <th>Date</th>
+        <th>Time</th>
+    </t>
     <?php
-    
-    $stmnt -> $db->prepare("SELECT * FROM tablename where STATEMENTS THAT MATCH THE USER ARE TRUE");
-    $stmnt -> execute();
-    while ($row = $stmnt-> fetch()) {
-        $l = $row['location'];
-        $c = $row['child'];
-        $d = $row['date'];
-        $t = $row['time'];
-        $id = $row['id'];
-        echo "<tr><td><input type='checkbox' name='check[]' value='$id'/></td>
-        <td>$l</td><td>$c</td><td>$d</td><td>$t</td></tr>";
-    }
+		while ($rows = $resultSet->fetch_assoc()) {
     ?>
-    -->
+        <tr>
+            <td><input type='checkbox'></td>
+            <td><?php echo $rows['id']; ?></td>
+            <td><?php echo $rows['location']; ?></td>
+            <td><?php echo $rows['child_last']; ?></td>
+            <td><?php echo $rows['date']; ?></td>
+            <td><?php echo $rows['time']; ?></td>
+        </tr>
+    <?php
+        }
+    ?>
 </table>
 </p>
     <div class="submit-button">
