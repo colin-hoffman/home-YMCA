032aa13710f784e2ce9d5ac7cb176967db7293b2
colin-hoffman <66750992+colin-hoffman@users.noreply.github.com>
2022-04-06 10:02:42 -0400

    Changed title of EditReservation page

--- EditReservation.php
+++ EditReservation.php
@@ -20,7 +20,7 @@ $resultSet2 = $mysqli->query("SELECT * FROM dblocation");
 <html lang="en" dir="ltr">
   <head>
     <meta charset="UTF-8">
-    <title>Create Reservation</title>
+    <title>Edit Reservation</title>
     <link rel="stylesheet" href="stylesheetForm.css">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
    </head>
