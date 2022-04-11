0996dae641b03a81f844bdee166f9495dd2fbb16
colin-hoffman <66750992+colin-hoffman@users.noreply.github.com>
2022-04-11 09:42:05 -0400

    Created changePassword.php page

---
+++ changePassword.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="UTF-8">
+    <title>Login Form for YMCA Child Watch</title>
+    <link rel="stylesheet" href="stylesheetForm.css">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   </head>
+<body>
+  <div class="container">
+    <div class="title">Change Password</div>
+    <div class="content">
+        <br>
+        <p>Enter your account email here. We will send you a temporary password, which you will 
+            be able to change once you login again. Once you click on the confirmation button, you will be
+            redirected back to the Login page.
+        </p>
+        <div class="user-details">
+          <div class="input-box">
+            <span class="center"></span>
+            <span class="details">Email Address</span>
+            <span class="required"></span>
+            <input type="text"  placeholder="Enter your Email Address" required>
+          </div>
+        
+        </div>
+        <div class="submit-button">
+            <form action="guardianHomepage.html" method="get">
+              <input type="submit" value="Change Password">
+            </form>
+        </div>
+    </div>
+  </div>
+</body>
+</html>
