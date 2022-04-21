4fadf5192a1068909cd326c33abe2b877f11a721
colin-hoffman <66750992+colin-hoffman@users.noreply.github.com>
2022-04-20 20:35:16 -0400

    Update changePassword.php

--- changePassword.php
+++ changePassword.php
@@ -1,35 +1,82 @@
-<!DOCTYPE html>
+<?php
+include_once('database/dbinfo.php');
+$mysqli = connect();
+$email = $_SESSION['_id'];
+
+$userEmail = $mysqli->query("SELECT email FROM dbpersons WHERE id='$email'");
+$emailString = $userEmail->fetch_assoc();
+$email = $emailString['email'];
+
+//$resultSet = $mysqli->query("SELECT password FROM dbpersons WHERE guardian_email='$email'");
+
+?>
+
+
 <html lang="en" dir="ltr">
   <head>
     <meta charset="UTF-8">
-    <title>Login Form for YMCA Child Watch</title>
+    <title>Change Password</title>
     <link rel="stylesheet" href="stylesheetForm.css">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
    </head>
 <body>
-  <div class="container">
-    <div class="title">Change Password</div>
-    <div class="content">
-        <br>
-        <p>Enter your account email here. We will send you a temporary password, which you will 
-            be able to change once you login again. Once you click on the confirmation button, you will be
-            redirected back to the Login page.
-        </p>
-        <div class="user-details">
-          <div class="input-box">
-            <span class="center"></span>
-            <span class="details">Email Address</span>
-            <span class="required"></span>
-            <input type="text"  placeholder="Enter your Email Address" required>
-          </div>
-        
-        </div>
-        <div class="submit-button">
-            <form action="guardianHomepage.html" method="get">
-              <input type="submit" value="Change Password">
-            </form>
-        </div>
-    </div>
-  </div>
-</body>
-</html>
+
+<div class="container">
+    <form action"" method="post">
+        <div class="title">Change Password</div>
+            <div class="content">
+                <div class="user-details">
+                    <div class="input-box">
+                        <span class="details">Current Password</span>
+                        <span class="required"></span>
+                        <input type="text" required>
+                    </div>
+                    <div class="input-box">
+                        <span class="details">New Password</span>
+                        <span class="required"></span>
+                        <input type="text" required>
+                    </div>
+                    <div class="input-box">
+                        <span class="details">Confirm Password</span>
+                        <span class="required"></span>
+                        <input type="text" required>
+                    </div>
+                    </div>
+          <div class="submit-button">
+          <form action="generalHomepage.html" method="get">
+            <input type="submit" value="Change Password">
+          </form>
+          <ul class="topnav">
+        <li><a class="active" href="http://localhost/home-YMCA/index.php">Home</a></li>
+        <li><a class="dark" href="http://localhost/home-YMCA/CreateLocation.php">Create Reser</a></li>
+        <li><a class="gray" href="#users">Create Users</a></li>
+        <li><a class="dark" href="#viewChildrenInfo">View Children Info</a></li>
+        <li><a class="gray" href="http://localhost/home-YMCA/personSearch.php"> Search People</a></li>
+        <li><a class="dark" href="http://localhost/home-YMCA/logout.php">Sign Out</a></li>
+      </ul>
+            </div>
+
+
+            <?php
+/* 	if(isset($_POST['_submit_check'])) {
+		
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+		
+		//Check for password being in use
+		$check_copy = $mysqli->query("SELECT email, password FROM dbpersons WHERE email = '$email' AND password = '$password'");
+		
+		//first check for copy
+	        if($check_copy->num_rows > 0) {
+			echo "$password has already been used for $email.";
+
+		//Change Password
+		} else {
+				
+			$final_result = "UPDATE dbreservation SET password = $newpassword WHERE email = $email")
+
+			if($mysqli->query($final_result) === TRUE){
+				echo "You have successfully changed your password"; */
+
+
